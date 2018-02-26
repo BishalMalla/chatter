be4c77c7e2e9789eb29582aa2d5cc97071d4013a
Bishal Malla <lxixchg@gmail.com>
2018-02-26 15:52:21 -0700

    added things back

--- server.js
+++ server.js
@@ -11,29 +11,29 @@ console.log('server running')
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + '/index.html')
 })
-// io.sockets.on('connection',socket => {
-//   connections.push(socket)
-//   console.log('socket connected', connections.length)
-//   //disconnect
-//   socket.on('disconnect', data => {
-//     users.splice(users.indexOf(socket.userName), 1)
-//     updateUserNames()
-//     connections.splice(connections.indexOf(socket), 1)
-//     console.log('disconnected', connections.length)
-//   })
-//   //send message
-//   socket.on('send message',data => {
-//     console.log(data)
-//     io.sockets.emit('new message', {msg : data})
-//   })
-//   //New User
-//   socket.on('new user', (data,callback)=> {
-//     callback(true)
-//     socket.userName = data
-//     users.push(socket.userName)
-//     updateUserNames()
-//   })
-//   function updateUserNames(){
-//     io.sockets.emit('get users', users)
-//   }
-// })
+io.sockets.on('connection',socket => {
+  connections.push(socket)
+  console.log('socket connected', connections.length)
+  //disconnect
+  socket.on('disconnect', data => {
+    users.splice(users.indexOf(socket.userName), 1)
+    updateUserNames()
+    connections.splice(connections.indexOf(socket), 1)
+    console.log('disconnected', connections.length)
+  })
+  //send message
+  socket.on('send message',data => {
+    console.log(data)
+    io.sockets.emit('new message', {msg : data})
+  })
+  //New User
+  socket.on('new user', (data,callback)=> {
+    callback(true)
+    socket.userName = data
+    users.push(socket.userName)
+    updateUserNames()
+  })
+  function updateUserNames(){
+    io.sockets.emit('get users', users)
+  }
+})
